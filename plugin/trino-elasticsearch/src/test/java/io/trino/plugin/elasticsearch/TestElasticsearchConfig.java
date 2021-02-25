49e1e8d5524b55cdff9cfd3bc76ab68b74f330db
周平 <parker.zhou@eoitek.com>
2021-02-25 16:21:46 +0800

    Elasticsearch Connector support applyAggregation for field can be predicated
    Support term bucket aggregation for group by set
    Support max, min, avg, sum, count(x), count(*)
    Session config `aggregation_pushdown_enabled` to control whether should apply aggregation

--- plugin/trino-elasticsearch/src/test/java/io/trino/plugin/elasticsearch/TestElasticsearchConfig.java
+++ plugin/trino-elasticsearch/src/test/java/io/trino/plugin/elasticsearch/TestElasticsearchConfig.java
@@ -35,6 +35,7 @@ public class TestElasticsearchConfig
     public void testDefaults()
     {
         assertRecordedDefaults(recordDefaults(ElasticsearchConfig.class)
+                .setAggregationPushdownEnabled(false)
                 .setHost(null)
                 .setPort(9200)
                 .setDefaultSchema("default")
@@ -64,6 +65,7 @@ public class TestElasticsearchConfig
         Path truststoreFile = Files.createTempFile(null, null);
 
         Map<String, String> properties = new ImmutableMap.Builder<String, String>()
+                .put("aggregation-pushdown.enabled", "true")
                 .put("elasticsearch.host", "example.com")
                 .put("elasticsearch.port", "9999")
                 .put("elasticsearch.default-schema-name", "test")
@@ -86,6 +88,7 @@ public class TestElasticsearchConfig
                 .build();
 
         ElasticsearchConfig expected = new ElasticsearchConfig()
+                .setAggregationPushdownEnabled(true)
                 .setHost("example.com")
                 .setPort(9999)
                 .setDefaultSchema("test")
